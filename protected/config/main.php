ba27f7c88852e926ca6e9f0d9c80ccb0c5f25d35
carsoto <carsoto8691@gmail.com>
2017-09-06 11:05:55 -0400

    Cambié el nombre del proyecto

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Curso de GIT',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -80,6 +80,6 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'adminEmail'=>'webmaster@example.com',
+		'adminEmail'=>'carsoto8691@gamil.com',
 	),
 );
