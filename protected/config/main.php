822dc63a3d19fce9d35c77c83cc2fab475f8f792
carsoto <carsoto8691@gmail.com>
2017-09-06 15:18:14 -0400

    Modifiqué el nombre del proyecto

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Curso GIT Rama 2',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
